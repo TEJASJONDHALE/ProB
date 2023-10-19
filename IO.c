1f4cc5bc8e495f6426503bad8cf8cab32acace40
Tejas Jondhale <97272393+TEJASJONDHALE@users.noreply.github.com>
2023-10-19 13:32:17 +0530

    Update IO.c

--- IO.c
+++ IO.c
@@ -1,32 +1,27 @@
-#include <stdio.h>
+#include<stdio.h>
 int main()
 {
-    int sumOdd = 0;
-    int sumEven = 0;
+    int sumodd=0;
+    int sumeven=0;
     int upperbound;
-    int absDiff;
-    int number;
-
-    printf("Enter the upperbound");
-    scanf("%d", &upperbound);
-
-    while (number <= upperbound)
+    int absdiff;
+    printf("enter the upper bound:");
+    scanf("%d",&upperbound);
+    for(int number=1;number<=upperbound;++number)
+    if(number%2==0)
     {
-        if (number % 2 == 0)
-        {
-            sumEven += number;
-        }
-        else
-        {
-            sumOdd += number;
-        }
-        ++number;
+        sumeven+=number;
     }
-    if (sumOdd == sumEven)
+    else
     {
-        absDiff = sumEven - sumOdd;
+        sumodd+=number;
     }
-
+    absdiff=(sumodd>sumeven)?(sumodd-sumeven):(sumeven-sumodd);
+    printf("the sumodd is %d.\n",sumodd);
+    printf("the sumeven is %d.\n",sumeven);
+    printf("the absdiff is %d.\n",absdiff);
+    return 0;
+}
     printf("The sum of Even numbers is %d.\n", sumEven);
     printf("The sum of Odd numbers is %d.\n", sumOdd);
     printf("The absolute difference is %d.\n", absDiff);
